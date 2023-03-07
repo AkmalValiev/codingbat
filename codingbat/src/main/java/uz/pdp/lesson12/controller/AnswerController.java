2ac8224d2ab896d4b1e5a9463d95c312e7ac7fa8
AkmalValiev <akmalvaliev86@gmail.com>
2023-03-07 20:02:54 +0500

    thirdCommit

--- codingbat/src/main/java/uz/pdp/lesson12/controller/AnswerController.java
+++ codingbat/src/main/java/uz/pdp/lesson12/controller/AnswerController.java
@@ -1,15 +1,20 @@
 package uz.pdp.lesson12.controller;
 
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import uz.pdp.lesson12.entity.Answer;
 import uz.pdp.lesson12.payload.AnswerDto;
 import uz.pdp.lesson12.payload.ApiResponse;
 import uz.pdp.lesson12.service.AnswerService;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/answer")
@@ -29,13 +34,13 @@ public class AnswerController {
     }
 
     @PostMapping
-    public ResponseEntity<ApiResponse> addAnswer(@RequestBody AnswerDto answerDto){
+    public ResponseEntity<ApiResponse> addAnswer(@Valid @RequestBody AnswerDto answerDto){
         ApiResponse apiResponse = answerService.addAnswer(answerDto);
         return ResponseEntity.status(apiResponse.isSuccess()? HttpStatus.CREATED:HttpStatus.CONFLICT).body(apiResponse);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<ApiResponse> editAnswer(@PathVariable Integer id, @RequestBody AnswerDto answerDto){
+    public ResponseEntity<ApiResponse> editAnswer(@PathVariable Integer id, @Valid @RequestBody AnswerDto answerDto){
         ApiResponse apiResponse = answerService.editAnswer(id, answerDto);
         return ResponseEntity.status(apiResponse.isSuccess()?HttpStatus.ACCEPTED:HttpStatus.CONFLICT).body(apiResponse);
     }
@@ -44,5 +49,17 @@ public class AnswerController {
     public ResponseEntity<ApiResponse> deleteAnswer(@PathVariable Integer id){
         ApiResponse apiResponse = answerService.deleteAnswer(id);
         return ResponseEntity.status(apiResponse.isSuccess()?HttpStatus.ACCEPTED:HttpStatus.CONFLICT).body(apiResponse);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex){
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error)->{
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
     }
 }
